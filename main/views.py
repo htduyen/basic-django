b20b979a628b061d918cf57f79e038403c1195fd
htduyen <thanhduyeny1@gmail.com>
2021-07-16 22:16:28 +0700

    Using redirect and render form to view

--- main/views.py
+++ main/views.py
@@ -1,16 +1,31 @@
 # Create your views here.
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 from main.models import TodoList
+from .forms import CreateNewList
 
 
 def index(response, id):
     ls = TodoList.objects.get(id=id)
-    # item = ls.item_set.get(id=2)
     return render(response, "main/list.html", dict(ls=ls))
 
 
 def home(response):
-    return render(response, "main/home.html")
+    return render(response, "main/home.html", {})
+
+
+def create(response):
+    if response.method == "POST":
+        form = CreateNewList(response.POST)
+        if form.is_valid():
+            n = form.cleaned_data["name"]
+            t = TodoList(name=n)
+            t.save()
+        return HttpResponseRedirect(f"/{t.id}")
+
+    else:
+        form = CreateNewList()
+
+    return render(response, "main/create.html", {"form": form})
